72ab843bb3210de20f7135c12fd333ae29c75399
Alexander López <lopez.alexander@pucp.edu.pe>
2021-10-27 12:28:57 -0500

    Add files via upload

---
+++ src/Publisher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float64
+
+if __name__ == "__main__":
+
+  rospy.init_node('publisher')
+  pub = rospy.Publisher('counter', Float64, queue_size=1)
+  rate = rospy.Rate(2) # 5 Hz
+  
+  count = 0
+  
+  while not rospy.is_shutdown():
+    
+    result = Float64()
+    result.data = count
+    rospy.loginfo(str(result.data))
+    pub.publish(result)
+    count = count + 1
+    rate.sleep()
---
+++ src/Subscriber.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float64
+
+def funcion(msg): # Tipo callback
+  resultado = msg.data  
+  print resultado
+
+if __name__ == "__main__":
+
+  rospy.init_node('subscriber')
+  rospy.Subscriber('counter', Float64, funcion)
+  rospy.spin()
